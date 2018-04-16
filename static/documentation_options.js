f8055b947f0f15e46b55bb1a6baa6fc182dba673
Rion Brattig Correia <rionbr@gmail.com>
2018-04-16 16:47:20 -0400

    Docs for v0.0.3

--- static/documentation_options.js
+++ static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.0.2-alpha',
+    VERSION: '0.0.3-alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
