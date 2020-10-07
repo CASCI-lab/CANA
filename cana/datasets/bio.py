86ef512eec5cd41e979590182cf6f2e397ec4ece
Rion Brattig Correia <rionbr@indiana.edu>
2020-10-07 15:06:20 +0100

    load cell collective models
    functions to easily load the cell collective models

--- cana/datasets/bio.py
+++ cana/datasets/bio.py
@@ -82,7 +82,7 @@ def LEUKEMIA():
     Returns:
         (BooleanNetwork)
     """
-    return BooleanNetwork.from_file(_path + '/leukemia.txt', type='logical', name="Leukemia", keep_constants=True)
+    return BooleanNetwork.from_file(_path + '/leukemia.txt', type='logical', name="T-LGL Leukemia", keep_constants=True)
 
 
 def BREAST_CANCER():
@@ -93,7 +93,84 @@ def BREAST_CANCER():
     Returns:
         (BooleanNetwork)
     """
-    return BooleanNetwork.from_file(_path + '/breast_cancer.txt', type='logical', name="Breast Cancer", keep_constants=True)
+    return BooleanNetwork.from_file(_path + '/breast_cancer.txt', type='logical', name="ER+ Breast Cancer", keep_constants=True)
+
+
+_cell_collective_models = [
+    'Apoptosis Network',
+    'Arabidopsis thaliana Cell Cycle',
+    'Aurora Kinase A in Neuroblastoma',
+    'B bronchiseptica and T retortaeformis coinfection',
+    'B cell differentiation',
+    'Bordetella bronchiseptica',
+    'Bortezomib Responses in U266 Human Myeloma Cells',
+    'BT474 Breast Cell Line Long-term ErbB Network',
+    'BT474 Breast Cell Line Short-term ErbB Network',
+    'Budding Yeast Cell Cycle 2009',
+    'Budding Yeast Cell Cycle',
+    'Cardiac development',
+    'CD4 T cell signaling',
+    'CD4+ T Cell Differentiation and Plasticity',
+    'CD4+ T cell Differentiation',
+    'Cell Cycle Transcription by Coupled CDK and Network Oscillators',
+    'Cholesterol Regulatory Pathway',
+    'Colitis-associated colon cancer',
+    'Cortical Area Development',
+    'Death Receptor Signaling',
+    'Differentiation of T lymphocytes',
+    'EGFR & ErbB Signaling',
+    'FA BRCA pathway',
+    'Fanconi anemia and checkpoint recovery',
+    'FGF pathway of Drosophila Signalling Pathways',
+    'Glucose Repression Signaling 2009',
+    'Guard Cell Abscisic Acid Signaling',
+    'HCC1954 Breast Cell Line Long-term ErbB Network',
+    'HCC1954 Breast Cell Line Short-term ErbB Network',
+    'HGF Signaling in Keratinocytes',
+    'HH Pathway of Drosophila Signaling Pathways',
+    'HIV-1 interactions with T Cell Signalling Pathway',
+    'Human Gonadal Sex Determination',
+    'IGVH mutations in chronic lymphocytic leukemia',
+    'IL-1 Signaling',
+    'IL-6 Signalling',
+    'Influenza A Virus Replication Cycle',
+    'Iron acquisition and oxidative stress response in aspergillus fumigatus',
+    'Lac Operon',
+    'Lymphoid and myeloid cell specification and transdifferentiation',
+    'Lymphopoiesis Regulatory Network',
+    'Mammalian Cell Cycle 2006',
+    'Mammalian Cell Cycle',
+    'MAPK Cancer Cell Fate Network',
+    'Metabolic Interactions in the Gut Microbiome',
+    'Neurotransmitter Signaling Pathway',
+    'Oxidative Stress Pathway',
+    'PC12 Cell Differentiation',
+    'Predicting Variabilities in Cardiac Gene',
+    'Pro-inflammatory Tumor Microenvironment in Acute Lymphoblastic Leukemia',
+    'Processing of Spz Network from the Drosophila Signaling Pathway',
+    'Regulation of the L-arabinose operon of Escherichia coli',
+    'Senescence Associated Secretory Phenotype',
+    'Septation Initiation Network',
+    'Signal Transduction in Fibroblasts',
+    'Signaling in Macrophage Activation',
+    'Signaling Pathway for Butanol Production in Clostridium beijerinckii NRRL B-598',
+    'SKBR3 Breast Cell Line Long-term ErbB Network',
+    'SKBR3 Breast Cell Line Short-term ErbB Network',
+    'Stomatal Opening Model',
+    'T cell differentiation',
+    'T Cell Receptor Signaling',
+    'T-Cell Signaling 2006',
+    'T-LGL Survival Network 2008',
+    'T-LGL Survival Network 2011 Reduced Network',
+    'T-LGL Survival Network 2011',
+    'TOL Regulatory Network',
+    'Toll Pathway of Drosophila Signaling Pathway',
+    'Treatment of Castration-Resistant Prostate Cancer',
+    'Trichostrongylus retortaeformis',
+    'Tumour Cell Invasion and Migration',
+    'VEGF Pathway of Drosophila Signaling Pathway',
+    'Wg Pathway of Drosophila Signalling Pathways',
+    'Yeast Apoptosis']
 
 
 def load_cell_collective_model(name=None):
@@ -104,86 +181,26 @@ def load_cell_collective_model(name=None):
             Accepts: ["Apoptosis Network", "Arabidopsis thaliana Cell Cycle", "Aurora Kinase A in Neuroblastoma", ...,
             "Wg Pathway of Drosophila Signalling Pathways", "Yeast Apoptosis"]
 
-    Note: see source code for full list of models.
+    Returns:
+        (BooleanNetwork)
+
+    Note: see source code for full list of models. Credits to Xuan Wang for compiling these models.
     """
-    models = [
-        'Apoptosis Network',
-        'Arabidopsis thaliana Cell Cycle',
-        'Aurora Kinase A in Neuroblastoma',
-        'B bronchiseptica and T retortaeformis coinfection',
-        'B cell differentiation',
-        'Bordetella bronchiseptica',
-        'Bortezomib Responses in U266 Human Myeloma Cells',
-        'BT474 Breast Cell Line Long-term ErbB Network',
-        'BT474 Breast Cell Line Short-term ErbB Network',
-        'Budding Yeast Cell Cycle 2009',
-        'Budding Yeast Cell Cycle',
-        'Cardiac development',
-        'CD4 T cell signaling',
-        'CD4+ T Cell Differentiation and Plasticity',
-        'CD4+ T cell Differentiation',
-        'Cell Cycle Transcription by Coupled CDK and Network Oscillators.',
-        'Cholesterol Regulatory Pathway',
-        'Colitis-associated colon cancer',
-        'Cortical Area Development',
-        'Death Receptor Signaling',
-        'Differentiation of T lymphocytes',
-        'EGFR & ErbB Signaling',
-        'FA BRCA pathway',
-        'Fanconi anemia and checkpoint recovery',
-        'FGF pathway of Drosophila Signalling Pathways',
-        'Glucose Repression Signaling 2009',
-        'Guard Cell Abscisic Acid Signaling',
-        'HCC1954 Breast Cell Line Long-term ErbB Network',
-        'HCC1954 Breast Cell Line Short-term ErbB Network',
-        'HGF Signaling in Keratinocytes',
-        'HH Pathway of Drosophila Signaling Pathways',
-        'HIV-1 interactions with T Cell Signalling Pathway',
-        'Human Gonadal Sex Determination',
-        'IGVH mutations in chronic lymphocytic leukemia',
-        'IL-1 Signaling',
-        'IL-6 Signalling',
-        'Influenza A Virus Replication Cycle',
-        'Iron acquisition and oxidative stress response in aspergillus fumigatus',
-        'Lac Operon',
-        'Lymphoid and myeloid cell specification and transdifferentiation',
-        'Lymphopoiesis Regulatory Network',
-        'Mammalian Cell Cycle 2006',
-        'Mammalian Cell Cycle',
-        'MAPK Cancer Cell Fate Network',
-        'Metabolic Interactions in the Gut Microbiome',
-        'Neurotransmitter Signaling Pathway',
-        'Oxidative Stress Pathway',
-        'PC12 Cell Differentiation',
-        'Predicting Variabilities in Cardiac Gene',
-        'Pro-inflammatory Tumor Microenvironment in Acute Lymphoblastic Leukemia',
-        'Processing of Spz Network from the Drosophila Signaling Pathway',
-        'Regulation of the L-arabinose operon of Escherichia coli',
-        'Senescence Associated Secretory Phenotype',
-        'Septation Initiation Network',
-        'Signal Transduction in Fibroblasts',
-        'Signaling in Macrophage Activation',
-        'Signaling Pathway for Butanol Production in Clostridium beijerinckii NRRL B-598',
-        'SKBR3 Breast Cell Line Long-term ErbB Network',
-        'SKBR3 Breast Cell Line Short-term ErbB Network',
-        'Stomatal Opening Model',
-        'T cell differentiation',
-        'T Cell Receptor Signaling',
-        'T-Cell Signaling 2006',
-        'T-LGL Survival Network 2008',
-        'T-LGL Survival Network 2011 Reduced Network',
-        'T-LGL Survival Network 2011',
-        'TOL Regulatory Network',
-        'Toll Pathway of Drosophila Signaling Pathway',
-        'Treatment of Castration-Resistant Prostate Cancer',
-        'Trichostrongylus retortaeformis',
-        'Tumour Cell Invasion and Migration',
-        'VEGF Pathway of Drosophila Signaling Pathway',
-        'Wg Pathway of Drosophila Signalling Pathways',
-        'Yeast Apoptosis']
+
     #
-    if name not in models:
-        models_str = "'" + "','".join(models) + "'"
-        raise TypeError('Model name could not be found. Please specify one of the following models: {models:s}'.format(models=models_str))
+    if name not in _cell_collective_models:
+        models_str = "'" + "','".join(_cell_collective_models) + "'"
+        raise TypeError('Model name could not be found. Please specify one of the following models: {models:s}.'.format(models=models_str))
     else:
         return BooleanNetwork.from_file(_path + '/cell_collective/' + name + '.txt', name=name, keep_constants=True)
+
+
+def load_all_cell_collective_models():
+    """Load all the Cell Collective models, instanciating +70 models.
+
+    Returns:
+        (list)
+
+    Note: see source code for full list of models.
+    """
+    return [load_cell_collective_model(name=name) for name in _cell_collective_models]
